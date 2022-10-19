ba9fd39c993cb9eadc13557fdd533294d2e12240
nitayke <nitayke@gmail.com>
2022-10-19 22:58:57 +0300

    first-commit

---
+++ code.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAm7hTWEmt1JRX3JXpq_dDNQWo0KKKC008",
+    authDomain: "shvushon2.firebaseapp.com",
+    databaseURL: "https://shvushon2-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "shvushon2",
+    storageBucket: "shvushon2.appspot.com",
+    messagingSenderId: "833572668563",
+    appId: "1:833572668563:web:efd646d875de094f203966",
+    measurementId: "G-VTT9E38KE8"
+  };
+  
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
